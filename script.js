e5083c20c9a6cda5f095932d399ec063cc533c09
Jaida1022 <43240424+Jaida1022@users.noreply.github.com>
2019-03-07 08:29:26 -0500

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,11 @@
+$("#WhiteCanary").click(function(){
+  $(".WhiteCanary").fadeToggle();
+});
+
+$("#Atom").click(function(){
+  $(".Atom").slideToggle();
+});
+
+$("#Firestorm").click(function(){
+  $(".Firestorm").toggle();
+});
